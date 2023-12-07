7d9aaf084b2e799c9fd8a83e3d1d5831e9ce1cab
olek2303 <olek230303@gmail.com>
2023-12-07 12:27:55 +0100

    added steps and possible methods to implement

--- main.py
+++ main.py
@@ -5,3 +5,17 @@ import seaborn as sns
 
 iris = pd.read_csv("./datasets/Iris.csv")
 print(iris.head())
+print(iris.shape)
+
+# 1. Naprawa danych (zamiana na wartości liczbowe, itp.)
+
+# Metody selekcji zmiennych
+# 1. Metoda eliminacji
+# 2. Metoda wstecznej eliminacji (backward elimination) (zachlanna minimalizacja)
+# 3. PCA
+
+# Analiza zbudowanego modelu
+# 1. Test Anova - test wariancji
+# 2. Regresja Ridge
+# 3. Pokazanie korelacji
+# 4. R-squared
